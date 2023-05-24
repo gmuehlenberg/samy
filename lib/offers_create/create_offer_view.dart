9f9b32f99c22098b20dc1330cadc2b1e344ed157
Georg Mühlenberg <georg.muehlenberg@gmail.com>
2023-05-24 11:29:00 +0200

    repair create offer page a bit

--- lib/offers_create/create_offer_view.dart
+++ lib/offers_create/create_offer_view.dart
@@ -7,89 +7,80 @@ import 'package:flutter/material.dart' hide View;
 
 // ignore: must_be_immutable
 class CreateOfferView extends View<CreateOfferMessage, CreateOfferModel, CreateOfferUpdate> {
-
-  CreateOfferView({Key? key}) : super(key: key);
-
-  SchoolType?_selectedSchooltype;
-  int? _selectedClassNo;
+  const CreateOfferView({Key? key}) : super(key: key);
 
   @override
-  Widget buildView(CreateOfferModel model) => Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: ListView(
-              children: [
-                Text(
-                  'Welcome to SAMY',
-                  style: TextStyle(fontSize: 25, color: Colors.blue[900]),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Create offer',
-                  style: TextStyle(color: Colors.grey[700], fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 30,
-                          child: DropdownButton<SchoolType>(
-                            items: SchoolType.values.map((type) => DropdownMenuItem(value: type, child: Text(type.name))).toList(),
-                            onChanged: (newItem) => _selectedSchooltype = newItem,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Expanded(
-                          flex: 30,
-                          child: DropdownButton<int>(
-                            items: List<int>.generate(13, (i) => i + 1).map((no) => DropdownMenuItem(value: no, child: Text(no.toString()))).toList(),
-                            onChanged: (newNo) => _selectedClassNo = newNo,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      padding: const EdgeInsets.all(15),
-                      backgroundColor: Colors.blue[900],
-                    ),
-                    child: const Text(
-                      'Tauschgesuch erstellen',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    onPressed: () {
-                      dispatch(
-                        CreateOffer(
-                          schoolType: _selectedSchooltype!,
-                          classNo: _selectedClassNo!,
-                          ),
-                        );
-                      // PopUp-Fenster anzeigen
-                      // NavigateTo anderer Page
-                    },
+  Widget buildView(CreateOfferModel model) => ListView(
+        padding: const EdgeInsets.all(30),
+        children: [
+          Text(
+            'Create offer',
+            style: TextStyle(color: Colors.grey[700], fontSize: 18),
+          ),
+          CreateOffersForm(),
+        ],
+      );
+}
 
-                  ),
+// ignore: use_key_in_widget_constructors
+class CreateOffersForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CreateOffersFormState();
+}
+
+class CreateOffersFormState extends State<CreateOffersForm> {
+  CreateOffersFormState();
+
+  SchoolType selectedSchooltype = SchoolType.Grundschule;
+  int selectedClassNo = 1;
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: DropdownButton<SchoolType>(
+                  items: SchoolType.values.map((type) => DropdownMenuItem(value: type, child: Text(type.name))).toList(),
+                  onChanged: (newItem) => setState(() {
+                    selectedSchooltype = newItem!;
+                  }),
+                  value: selectedSchooltype,
                 ),
-              ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: DropdownButton<int>(
+                  items: List<int>.generate(13, (i) => i + 1).map((no) => DropdownMenuItem(value: no, child: Text(no.toString()))).toList(),
+                  onChanged: (newNo) => setState(() {
+                    selectedClassNo = newNo!;
+                  }),
+                  value: selectedClassNo,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue[900],
+              ),
+              child: const Text(
+                'Tauschgesuch erstellen',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              onPressed: () {
+                dispatch(
+                  CreateOffer(
+                    schoolType: selectedSchooltype,
+                    classNo: selectedClassNo,
+                  ),
+                );
+              },
             ),
           ),
-        ),
+        ],
       );
 }
